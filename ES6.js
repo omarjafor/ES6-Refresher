86847d64ccfbb176adb0697bd6bd72df760250f8
omarjafor <omarjaforchy@gmail.com>
2022-11-06 21:24:06 +0600

    ES6 Refresher

--- ES6.js
+++ ES6.js
@@ -4,12 +4,25 @@
 // console.log(x+y);
 // const myName = "jaberjabed";
 // console.log(myName);
+
 // function num(name){
 //     let num2 = name*2;
 //     console.log(num2);
 // }
 // num(6);
+
 // const numMul = num => console.log(num*2);
 // numMul(5);
+
 // const numMul2 = (num,num2) => console.log(num*num2);
 // numMul2(5,6);
+
+import {text, setText} from './myModule.js';
+console.log(text);
+setText("Good Bye From ES6");
+console.log(text);
+setText("Welcome Back by Jaberjabed");
+import {message} from './myModule.js';
+console.log(message);
+import person from './myModule.js';
+console.log(person);
