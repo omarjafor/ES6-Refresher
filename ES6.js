fa77d76736cfe5e8a479694e2c62afbb896035c0
omarjafor <omarjaforchy@gmail.com>
2022-11-07 18:18:35 +0600

    ES Refreshing

--- ES6.js
+++ ES6.js
@@ -1,28 +1,28 @@
 // ES6 Refresher ----> Doing some practice again es6
-// let x = 5;
-// let y = 6;
-// console.log(x+y);
-// const myName = "jaberjabed";
-// console.log(myName);
+let x = 5;
+let y = 6;
+console.log(x+y);
+const myName = "jaberjabed";
+console.log(myName);
 
-// function num(name){
-//     let num2 = name*2;
-//     console.log(num2);
-// }
-// num(6);
+function num(name){
+    let num2 = name*2;
+    console.log(num2);
+}
+num(6);
 
-// const numMul = num => console.log(num*2);
-// numMul(5);
+const numMul = num => console.log(num*2);
+numMul(5);
 
-// const numMul2 = (num,num2) => console.log(num*num2);
-// numMul2(5,6);
+const numMul2 = (num,num2) => console.log(num*num2);
+numMul2(5,6);
 
-import {text, setText} from './myModule.js';
-console.log(text);
-setText("Good Bye From ES6");
-console.log(text);
-setText("Welcome Back by Jaberjabed");
-import {message} from './myModule.js';
-console.log(message);
-import person from './myModule.js';
-console.log(person);
+// import {text, setText} from './myModule.js';
+// console.log(text);
+// setText("Good Bye From ES6");
+// console.log(text);
+// setText("Welcome Back by Jaberjabed");
+// import {message} from './myModule.js';
+// console.log(message);
+// import person from './myModule.js';
+// console.log(person);
