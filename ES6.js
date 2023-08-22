bf30e2d02203d1dc89bd02921e80d1cf43eea47b
omarjafor <omarjaforchy@gmail.com>
2023-08-22 21:09:42 +0600

    here is again es6

--- ES6.js
+++ ES6.js
@@ -1,15 +1,15 @@
 // ES6 Refresher - Doing some practice again es6
 let x = 5;
-let y = 6;
+let y = 5;
 console.log(x+y);
 const myName = "jaberjabed";
 console.log(myName);
 
-function num(name){
-    let num2 = name*2;
+function num(num){
+    let num2 = num*2;
     console.log(num2);
 }
-num(6);
+num(5);
 
 const numMul = num => console.log(num*2);
 numMul(5);
