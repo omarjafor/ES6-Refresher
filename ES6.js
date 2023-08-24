de7d2026954603d1bb0c156ecb39732d49ae8884
omarjafor <omarjaforchy@gmail.com>
2023-08-24 23:00:27 +0600

    problem solve

--- ES6.js
+++ ES6.js
@@ -288,17 +288,3 @@ console.log(min( [1,3,2 ]));
 
 const cube=x=> x*x*x; 
 console.log(cube(2))
-
-const [a, b] = [1,2,3,4,45,5]; 
-console.log(a+b);
-
-const {x, y, z} = {x: 1, y: 2, z: 3};
-console.log(y);
-
-const nums = [1,2,3,4,5];
-let output = nums.filter(n => n%2);
-console.log(output);
-
-const friendss = ["Moushumi", "Misha", "Manna", "mimi" , "mahiya"];
-const newFrnd = friendss.filter(friend=>friend.length==5);
-console.log(newFrnd);
