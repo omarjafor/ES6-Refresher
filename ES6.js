a1dcc847561f859ffe841b81caf67606029c8fbb
omarjafor <omarjaforchy@gmail.com>
2023-08-24 18:59:20 +0600

    solve quiz

--- ES6.js
+++ ES6.js
@@ -282,3 +282,23 @@ const margeTwo = (array1, array2) => {
     return Math.max(...array);
 }
 margeTwo(abc1, abc2)
+
+function min(nums) { return Math.min(...nums) }
+console.log(min( [1,3,2 ]));
+
+const cube=x=> x*x*x; 
+console.log(cube(2))
+
+const [a, b] = [1,2,3,4,45,5]; 
+console.log(a+b);
+
+const {x, y, z} = {x: 1, y: 2, z: 3};
+console.log(y);
+
+const nums = [1,2,3,4,5];
+let output = nums.filter(n => n%2);
+console.log(output);
+
+const friendss = ["Moushumi", "Misha", "Manna", "mimi" , "mahiya"];
+const newFrnd = friendss.filter(friend=>friend.length==5);
+console.log(newFrnd);
