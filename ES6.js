abc82a4730034afa46c445ba5f4b6b1fa9089c33
omarjafor <omarjaforchy@gmail.com>
2023-08-26 10:11:48 +0600

    Task 1

--- ES6.js
+++ ES6.js
@@ -486,3 +486,26 @@ mahindra.activity();
 console.log(mahindra['name']);
 console.log(mahindra['price']);
 */
+// task -1: console log the secondary school location of Sophia 
+let data = {
+    Sophia: {
+        id: 33,
+        study: [
+            {
+                primary:
+                    [
+                        { school_name: "ABC primary school" },
+                        { location: "Peters burg" }
+                    ]
+            },
+            {
+                secondary:
+                    [
+                        { school_name: "ABC secondary school" },
+                        { location: "St Lorence" }
+                    ]
+            },
+        ]
+    }
+}
+console.log(data.Sophia.study[1].secondary[1].location);
