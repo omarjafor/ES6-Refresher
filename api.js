dfbd70765f76067b9cbfb3c1291d28dbf9f197de
omarjafor <omarjaforchy@gmail.com>
2023-08-27 16:19:56 +0600

    api json

---
+++ api.js
@@ -0,0 +1,10 @@
+//JavaScript Object Notation (JSON)
+const people = [
+    { name: 'Mina', age: 20, },
+    { name: 'Rina', age: 15, },
+    { name: 'Suchorita', age: 22, }
+]
+const stringyfy = JSON.stringify(people);
+const givenNum = [33, 50, 79, 78, 90, 101, 30];
+console.log(people);
+console.log(stringyfy);
