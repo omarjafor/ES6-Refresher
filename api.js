e1d4ef358aab3d2676e1a707fa432f8d98c4a37e
omarjafor <omarjaforchy@gmail.com>
2023-08-27 16:41:48 +0600

    fetch api

--- api.js
+++ api.js
@@ -17,3 +17,9 @@ function loadData(){
         .then(response => response.json())
         .then(json => console.log(json))
 }
+
+function loadData2(){
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(res => res.json())
+    .then(json => console.log(json))
+}
