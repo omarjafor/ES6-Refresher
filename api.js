f2eef222369b0928ae4c6d5946b7b52f025b31a6
omarjafor <omarjaforchy@gmail.com>
2023-08-28 10:21:32 +0600

    data fetch

--- api.js
+++ api.js
@@ -99,3 +99,25 @@ function displayTodos(datas) {
     }
 }
 loadTodos()
+
+const dreamGirl = [
+    {
+        sokina: {
+            name: "bbu",
+            height: "5.4",
+            family: [{ father: "rock", mother: "shila", sister: "chinki" }],
+            age: undefined,
+            contactInfo: [
+                {
+                    facebook: {
+                        link: "https://www.facebook.com/",
+                        followers: "12545",
+                        status: "single",
+                        friendsList: [{ name: "rofik" }, undefined],
+                    },
+                },
+                { instagram: "https://www.instagram.com/" },
+            ],
+        },
+    },
+];
