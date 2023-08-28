e7671502423c9abaeb7b37f8e1ca4b75c758e74b
omarjafor <omarjaforchy@gmail.com>
2023-08-28 10:40:42 +0600

    fetch api

--- api.js
+++ api.js
@@ -58,15 +58,17 @@ function loadPosts() {
 
 function displayPost(datas) {
     const postcontainer = document.getElementById('posts-container');
-        for (const post of datas) {
+    for (const post of datas) {
+        if (post.id <= 10) {
             const div = document.createElement('div');
             div.classList.add('post');
             div.innerHTML = `
-            <h4>User: ${post.userId} </h4>
+            <h4>User: ${post.id} </h4>
             <h5>Post: ${post.title} </h5>
             <p>Post Description: ${post.body} </p>
         `;
             postcontainer.appendChild(div);
+        }
     }
 }
 loadPosts()
@@ -80,16 +82,16 @@ function loadTodos() {
 function displayTodos(datas) {
     const postcontainer = document.getElementById('todos-container');
     for (const post of datas) {
-       if(post.id <= 10){
-           const div = document.createElement('div');
-           div.classList.add('post');
-           div.innerHTML = `
+        if (post.id <= 10) {
+            const div = document.createElement('div');
+            div.classList.add('post');
+            div.innerHTML = `
             <h4>User: ${post.id} </h4>
             <h5>Post: ${post.title} </h5>
             <p>Post User ID: ${post.userId} </p>
         `;
-           postcontainer.appendChild(div);
-       }
+            postcontainer.appendChild(div);
+        }
     }
 }
 loadTodos()
