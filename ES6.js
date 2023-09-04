8fcc0452e8a09987013ee91f2bbec5e38e88bc08
omarjafor <omarjaforchy@gmail.com>
2023-09-04 10:12:31 +0600

    es6

--- ES6.js
+++ ES6.js
@@ -651,3 +651,11 @@ taskOne(() => {
 });
 
 // Promise Part - 1 ........ Pending, Resolve, Rejected
+const sheIs = true;
+try{
+    if(sheIs){
+        console.log('Not he')
+    }
+}catch(err){
+    console.log(err);
+}
