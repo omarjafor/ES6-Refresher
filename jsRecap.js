78b9cbb1ccb50c140e611eabdc5550e21fb1dc89
omarjafor <omarjaforchy@gmail.com>
2023-09-05 19:40:22 +0600

    ES6 You need to know for react

--- jsRecap.js
+++ jsRecap.js
@@ -11,3 +11,18 @@ if(fatherName==='arnold' || season ==='winter'){
 const numbers = [89, 45, 85, 95, 15];
 numbers[1] = 65;
 
+for(let i=0; i<numbers.length; i++){
+    console.log(numbers[i]);
+}
+
+function multiply(num1, num2){
+    return num1*num2;
+}
+
+const student = {
+    name: 'Shakib Alom',
+    age: 32,
+    movies: ['jokkor bokkor', 'priyotoma', 'dhakarpola']
+}
+console.log(student.movies[1]);
+console.log(student['age']);
