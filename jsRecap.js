d5cb904e3da0bc3565ee0fd9be926acb61600565
omarjafor <omarjaforchy@gmail.com>
2023-09-05 19:34:11 +0600

    ES6 You need to know for react

---
+++ jsRecap.js
@@ -0,0 +1,9 @@
+const fatherName = 'Jafor Ullah';
+let season = 'summer';
+season = 'winter';
+
+if(fatherName==='arnold' || season ==='winter'){
+    console.log('Something is true');
+}else{
+    console.log('Nothings is true');
+}
