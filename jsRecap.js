e150295ac98332da9096229c4d61807913bb4935
omarjafor <omarjaforchy@gmail.com>
2023-10-08 18:08:23 +0600

    some practice

--- jsRecap.js
+++ jsRecap.js
@@ -114,3 +114,9 @@ const country = 'Bangladesh';
 const adventurer = { name: 'Alice', cat: { name: 'Lucy' } };
 const ratName = adventurer?.rat?.name;
 console.log(ratName);
+
+let person = null;
+console.log(person ? person : "person is null");
+
+const obj = { a: 1 };
+console.log(Object.keys(obj).length === 0);
