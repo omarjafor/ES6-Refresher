54509f2c6ff2fdbd098ed86db8b439c49d4af1aa
omarjafor <omarjaforchy@gmail.com>
2023-11-11 19:26:13 +0600

    es6 js refreshign practice

--- api.js
+++ api.js
@@ -118,12 +118,3 @@ const dreamGirl = [
     },
 ];
 console.log(dreamGirl[0].sokina.contactInfo[1].instagram)
-
-const phones = [
-    { name: "sony", price: 500 },
-    { name: "apple", price: 700 },
-    { name: "sony", price: 700 },
-];
-
-const phoness = phones.filter(p => p.price != 500);
-console.log(phoness);
