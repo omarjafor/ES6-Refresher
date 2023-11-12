0660d12e726a191a8d6b54644c0affd7d3aa847e
omarjafor <omarjaforchy@gmail.com>
2023-11-12 15:02:03 +0600

    es6 practice for interview

--- jsRecap.js
+++ jsRecap.js
@@ -8,12 +8,7 @@ season = 'winter';
 //     console.log('Nothings is true');
 // }
 
-const numbers = [89, 45, 85, 95, 15];
-numbers[1] = 65;
 
-for(let i=0; i<numbers.length; i++){
-    // console.log(numbers[i]);
-}
 
 function multiply(num1, num2){
     return num1*num2;
