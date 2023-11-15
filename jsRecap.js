0b2cc5849e7d8d6fb34bf39abc344cf52cb3af14
omarjafor <omarjaforchy@gmail.com>
2023-11-15 17:12:50 +0600

    js es6 practice with some coding

--- jsRecap.js
+++ jsRecap.js
@@ -2,6 +2,12 @@ const fatherName = 'Jafor Ullah';
 let season = 'summer';
 season = 'winter';
 
+// if(fatherName==='arnold' || season ==='winter'){
+//     console.log('Something is true');
+// }else{
+//     console.log('Nothings is true');
+// }
+
 const numbers = [89, 45, 85, 95, 15];
 numbers[1] = 65;
 
