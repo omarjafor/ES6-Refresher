19764fd689336275b925b44b7eb38313af8e3409
omarjafor <omarjaforchy@gmail.com>
2023-11-15 17:12:13 +0600

    js es6 practice with some coding

--- myModule.js
+++ myModule.js
@@ -1,14 +1,2 @@
 export let text = "Welcome To JS Module ES6";
 
-export function setText(txt){
-    text = txt;
-}
-
-export {text as message}
-
-const person1 = {
-    name : "Omar jafor chy",
-    age : 28,
-    mobile : "01829622667"
-}
-export default person1;
