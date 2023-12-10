b65e299bb331494fbfe25a7e4035aba7e8b7b03a
omarjafor <omarjaforchy@gmail.com>
2023-12-10 15:59:54 +0600

    some practice and excercise

--- jsRecap.js
+++ jsRecap.js
@@ -2,11 +2,11 @@ const fatherName = 'Jafor Ullah';
 let season = 'summer';
 season = 'winter';
 
-// if(fatherName==='arnold' || season ==='winter'){
-//     console.log('Something is true');
-// }else{
-//     console.log('Nothings is true');
-// }
+if(fatherName==='arnold' || season ==='winter'){
+    console.log('Something is true');
+}else{
+    console.log('Nothings is true');
+}
 
 const numbers = [89, 45, 85, 95, 15];
 numbers[1] = 65;
